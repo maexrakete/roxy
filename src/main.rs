58d73f2ba5ccb1efa1c612f045e9d77bf90171a0
Max Kiehnscherf <max.kiehnscherf@gmail.com>
2018-07-26 21:30:33 +0200

    [wip] working get-proxy

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
 extern crate hyper;
 extern crate futures;
 
-use hyper::{Body, Chunk, Client, Method, Response, Request, Server};
+use hyper::{Body, Client, Method, Response, Request, Server};
 use hyper::client::HttpConnector;
-use hyper::service::service_fn_ok;
-use hyper::rt::{self, Future};
+use hyper::service::service_fn;
+use futures::{future, Future};
 
 fn main() {
 
   let addr = ([127,0,0,1], 3000).into();
-  let client = Client::new();
-  let new_service = |client| {
-    service_fn_ok(|req| {
-      handle_request(req, client)
-    })
-  };
 
-  let server = Server::bind(&addr)
-    .serve(new_service)
-    .map_err(|e| eprintln!("server error: {}", e));
+  hyper::rt::run(future::lazy(move || {
+    let client = Client::new();
 
-  println!("Listening on http://{}", addr);
+    let new_service = move || {
+      let client = client.clone();
+      service_fn(move |req| {
+        handle_request(req, &client)
+      })
+    };
 
-  rt::run(server);
+    let server = Server::bind(&addr)
+      .serve(new_service)
+      .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    server
+  }));
 }
 
 fn handle_request(req: Request<Body>, client: &Client<HttpConnector>) -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send> {
+  let base_url = String::from("http://localhost:8080");
+  let proxy_url = base_url + req.uri().path();
   let proxy_request = Request::builder()
     .method(Method::GET)
-    .uri(format!("http://localhost:8000{}", req.uri()))
+    .uri(&proxy_url)
     .body(Body::from(""))
     .unwrap();
 
+  println!("{}", &proxy_url);
   let proxy_request_future = client.request(proxy_request);
 
   Box::new(proxy_request_future.map(|web_res| {
