8509ed6274bc5217fb35b4e43f1d89b68557963f
Max Kiehnscherf <max.kiehnscherf@21re.de>
2018-07-26 17:31:25 +0200

    [chore] implement basic server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 extern crate hyper;
 extern crate futures;
 
-
-use hyper::server::{Http};
-use request_handler::RequestHandler;
-
+use hyper::{Body, Response, Request, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::{self, Future};
 
 fn main() {
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(RequestHandler)).unwrap();
-    server.run().unwrap();    
+
+  let addr = ([127,0,0,1], 3000).into();
+
+  let new_service = || {
+    service_fn_ok(|_|{
+      Response::new(Body::from("fkbr"))
+    })
+  };
+
+  let server = Server::bind(&addr)
+    .serve(new_service)
+    .map_err(|e| eprintln!("server error: {}", e));
+
+  println!("Listening on http://{}", addr);
+
+  rt::run(server);
 }
 
+fn handleRequest(req: Request<Body>) -> Box<Future<Item=Response<Body>, Error=hyper::Error> + Send> {
 
-mod request_handler;
+}
